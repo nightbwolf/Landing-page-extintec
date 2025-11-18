d4c9a1b29591d027ed2cc9897a4639963aa373c4
Roberto <robertovitortroca766@gmail.com>
2025-11-17 22:13:19 -0300

    adicionado parte de scroll- a ser modificado, também foi feito a interação entre as páginas e colocado as informações da empresa

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,17 @@
-document.getElementById("").addEventListener("click", function(){
-    
-});
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+function showSlide(i){
+    slides.forEach(sl => sl.classList.remove('active'));
+    slides[i].classList.add('active');
+};
+
+document.querySelector('.next').onclick = () => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+};
+
+document.querySelector('.prev').onclick = () => {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index)
+};
